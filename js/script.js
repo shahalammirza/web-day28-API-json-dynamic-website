c396ee1cee75478971a31a40ad757f77be8ac3e4
shahalammirza <mshahalam1246@gmail.comm>
2026-03-02 00:20:29 +0600

    added primary file and creat a boject to a json string and json string to real object code

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let person = {
+    name:"shahalam",
+    age: 26,
+    fruit: 'mango',
+    dish: "halim",
+    friend: ['kolim','jolim', 'lolim'],
+    money: 15000
+}
+
+console.log(person);
+
+// this object convert into JSON
+// JSON.stringify
+
+const personJson = JSON.stringify(person);
+console.log(personJson, typeof personJson);
+
+// this json string convert to a real object 
+//JSON.parse
+
+const parseJson = JSON.parse(personJson);
+console.log(parseJson, typeof parseJson);
